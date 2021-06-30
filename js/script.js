9d7d4ae110c1ce4ba725e84ec07301278caf0a19
maxfixe258369 <70097773+maxfixe258369@users.noreply.github.com>
2021-06-30 16:42:31 +0700

    update

--- js/script.js
+++ js/script.js
@@ -8,12 +8,29 @@ function closeNav() {
 
 $(document).ready(function(){
     $(".button-more").click(function(){
+        $(".description").addClass("fly");
         $("html").animate(
             {
-              scrollTop: $("#description").offset().top
+              scrollTop: $(".description").offset().top
             },
             1000 //speed
           );      
     });
+});
+
+
+
+$(window).scroll(function () {
+    const scrollTop = $(window).scrollTop();
+
+    if (scrollTop == 400) {
+        $(".description").addClass("fly");
+        console.log('check');
+    }
+
+    if (scrollTop == 1400) {
+        $(".ourworks-image").addClass("animate");
+        console.log('check');
+    }
 });
 

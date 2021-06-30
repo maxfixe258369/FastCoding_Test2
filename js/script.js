6620bf8353b8395a7ce580b08ddcb490a5eb9363
maxfixe258369 <70097773+maxfixe258369@users.noreply.github.com>
2021-06-30 13:04:55 +0700

    Update

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+$(document).ready(function(){
+    $(".button-more").click(function(){
+        $("html").animate(
+            {
+              scrollTop: $("#description").offset().top
+            },
+            1000 //speed
+          );      
+    });
+});
+
